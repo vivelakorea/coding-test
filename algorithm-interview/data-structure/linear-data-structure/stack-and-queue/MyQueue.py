3611e139da0d30b42432b31f004e6d64ca7bbb6e
vivelakorea <gyujang95@snu.ac.kr>
2021-06-05 13:31:26 +0900

    Implement Queue using Stacks

---
+++ algorithm-interview/data-structure/linear-data-structure/stack-and-queue/MyQueue.py
@@ -0,0 +1,50 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.input, self.output = [], []
+        
+
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.input.append(x)
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        self.peek()
+        return self.output.pop()
+        
+
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        if not self.output:
+            while self.input:
+                self.output.append(self.input.pop())
+        return self.output[-1]
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        return not (self.input or self.output)
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+obj = MyQueue()
+obj.push(10)
+
+
+param_2 = obj.pop()
+
+param_4 = obj.empty()
+
+print(param_2, param_4)
