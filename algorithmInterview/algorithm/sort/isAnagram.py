5a71498a4de88d6db8f87a52ec62849c1c49bcbf
vivelakorea <gyujang95@snu.ac.kr>
2021-06-13 23:18:41 +0900

    Valid Anagram

---
+++ algorithmInterview/algorithm/sort/isAnagram.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        hashed1, hashed2 = 0, 0
+        for c in s:
+            hashed1 += hash(c)
+        for c in t:
+            hashed2 += hash(c)
+        print(hashed1, hashed2)
+        return hashed1 == hashed2
+
+s = Solution()
+print(s.isAnagram("anagram", "nagaram"))
+print(s.isAnagram("rat", "car"))
+print(s.isAnagram("a", "ab"))
