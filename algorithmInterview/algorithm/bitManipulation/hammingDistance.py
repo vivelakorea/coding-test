f132dac39687e690e3adff20b1e6a898ba0c17a2
vivelakorea <gyujang95@snu.ac.kr>
2021-06-17 15:13:32 +0900

    Hamming Distance

---
+++ algorithmInterview/algorithm/bitManipulation/hammingDistance.py
@@ -0,0 +1,10 @@
+class Solution:
+    def hammingDistance(self, x: int, y: int) -> int:
+        return bin(x ^ y).count('1')
+
+s = Solution()
+print(s.hammingDistance(1, 4))
+print(s.hammingDistance(3, 1))
+print(s.hammingDistance(0, 0))
+print(s.hammingDistance(1, 0))
+print(s.hammingDistance(0, 1))
