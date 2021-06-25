ef103e53ccb08bb824a52e15130b532a1995e7f9
vivelakorea <gyujang95@snu.ac.kr>
2021-06-25 23:17:28 +0900

    Longest Repeating Character Replacement

--- algorithmInterview/algorithm/slidingWindow/characterReplacement.py
+++ algorithmInterview/algorithm/slidingWindow/characterReplacement.py
@@ -2,28 +2,32 @@ import collections
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left, right = 0, len(s)
         res = 0
-        count = collections.Counter(s)
-        num_max_character = count.most_common()[0][1]
-        flag = True
-        while left <= right:
-            num_max_character = count.most_common()[0][1]
-            print(left, right, num_max_character)
-            if right - left - num_max_character <= k:
-                res = max(res, right - left)
-            if flag:
-                count[s[left]] -= 1
-                left += 1
-                flag = False
-            else:
-                count[s[right - 1]] -= 1
-                right -= 1
-                flag = True
+        # num_max_character = count.most_common()[0][1]
+        def expand(left, right):
+            count = collections.Counter(s[left:right+1])
+            most_common = count.most_common()[0][1]
+            while right < len(s) and right - left + 1 - most_common <= k:
+                right += 1
+                if right < len(s):
+                    count[s[right]] += 1
+                most_common = count.most_common()[0][1]
+                # print('left', left, 'right', right, 'most_common', most_common, 'count.most_common()', count.most_common())
+
+            # print('sdf',right - left)
+            return right - left
+        
+        for left in range(len(s)):
+            right = left + res
+            if right > len(s):
+                break
+            res = max(res, expand(left, right))
+
         return res
 
 
 s = Solution()
-# print(s.characterReplacement("ABAB", 2))
-# print(s.characterReplacement("AABABBA", 1))
+print(s.characterReplacement("ABAB", 2))
+print(s.characterReplacement("AABABBA", 1))
 print(s.characterReplacement("AAAB", 0))
+print(s.characterReplacement("AABA", 0))
