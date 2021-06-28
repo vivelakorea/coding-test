049a8330e8f8e1ba152f74f16f1588c1161faf9c
vivelakorea <gyujang95@snu.ac.kr>
2021-06-28 13:49:14 +0900

    p2504

--- baekjoon/p2504.py
+++ baekjoon/p2504.py
@@ -1,32 +1,38 @@
-line = input()
+class Solution:
+    def parenthesisScore(self, line):
+        char_stack = []
+        score_stack = [0]
 
-char_stack = []
-score_stack = [0]
+        balanced = True
 
-balanced = True
+        ref = {
+            ')': ('(', 2),
+            ']': ('[', 3)
+        }
 
-ref = {
-    ')': ('(', 2),
-    ']': ('[', 3)
-}
+        for c in line:
+            if c in ref:
+                if not char_stack or char_stack.pop() != ref[c][0]:
+                    return 0
+                else:
+                    if score_stack[-1] == 0:
+                        score_stack.pop()
+                        score_stack[-1] += ref[c][1]
+                    else:
+                        popped = score_stack.pop()
+                        score_stack[-1] += popped * ref[c][1]
+            else: # '(' or '['
+                char_stack.append(c)
+                score_stack.append(0)
 
-for c in line:
-    if c in ref:
-        if char_stack.pop() != ref[c][0]:
-            balanced = False
-            break
-        else:
-            if score_stack[-1] == 0:
-                score_stack.pop()
-                score_stack[-1] += ref[c][1]
-            else:
-                popped = score_stack.pop()
-                score_stack[-1] += popped * ref[c][1]
-    else: # '(' or '['
-        char_stack.append(c)
-        score_stack.append(0)
+        if char_stack:
+            return 0
+        return score_stack[0]
 
-if char_stack or not balanced:
-    print('0')
-else:
-    print(score_stack[0])
+s = Solution()
+print(s.parenthesisScore(input()))
+# print(s.parenthesisScore('(()[[]])([])'))
+# print(s.parenthesisScore('[][]((])'))
+# print(s.parenthesisScore('('))
+# print(s.parenthesisScore(')'))
+# print(s.parenthesisScore('()[()]'))
