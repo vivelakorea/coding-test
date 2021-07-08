c293e43386b83c95d3b2bc80038734f52827fc5d
vivelakorea <gyujang95@snu.ac.kr>
2021-07-08 19:15:20 +0900

    datascience intro homework

---
+++ dataScienceIntro/P3.py
@@ -0,0 +1,45 @@
+from typing import List
+                
+def P3(grid: List[List[str]]) -> int:
+    
+    remain = set() # set of all coordinates(tuple) of 1 that isn't visited
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if grid[r][c]:
+                remain.add((r, c))
+
+    
+    def DFS(v: tuple):
+        remain.remove(v)
+
+        r, c = v
+        if (r - 1, c) in remain:
+            DFS((r - 1, c))
+        if (r + 1, c) in remain:
+            DFS((r + 1, c))
+        if (r, c - 1) in remain:
+            DFS((r, c - 1))
+        if (r, c + 1) in remain:
+            DFS((r, c + 1))
+
+
+    count = 0
+    while True:
+        if not remain:
+            break
+
+        # https://stackoverflow.com/questions/59825/how-to-retrieve-an-element-from-a-set-without-removing-it
+        v = None
+        for w in remain:
+            v = w
+            break
+        
+        DFS(v)
+        count += 1
+
+    return count
+    
+
+print(P3([[1,1,1,1,0], [1,0,0,1,0], [1,1,0,1,0], [1,1,0,0,0]]))
+print(P3([[1,1,0,0,0], [1,1,0,0,0], [0,0,1,1,0], [0,0,0,0,1]]))
+print(P3([[0,1,0,1,0,1]]))
