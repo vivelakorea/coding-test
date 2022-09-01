7263fdd7f659bf3136159f429e3e36238dac0d9c
심규장 <simgyujang@simgyujang-ui-MacBookPro.local>
2022-09-01 22:21:22 +0900

    p10815

---
+++ baekjoon/p10815.py
@@ -0,0 +1,7 @@
+_ = input()
+cards_sangun_possess = set(input().split())
+_ = input()
+cards_shown = list(input().split())
+res = map(lambda x: str(int((x in cards_sangun_possess))) + ' ', cards_shown)
+
+print(''.join(list(res))[:-1])
